5656aa83bfcb02eb27f05263b23bfc02ed77ff43
Luan Bo <lblytz23@live.cn>
2020-04-23 06:38:18 +0900

    Initial commit

---
+++ config.py
@@ -0,0 +1,8 @@
+import os
+
+RAW_DATA_TRAIN = os.path.join('./', 'data', 'AutoMaster_TrainSet.csv')
+RAW_DATA_TEST = os.path.join('./', 'data', 'AutoMaster_TestSet.csv')
+RAW_DATA_TRAIN_TEST = os.path.join('./', 'data', 'AutoMaster_TrainSet_forTest.csv')
+RAW_DATA_TEST_TEST = os.path.join('./', 'data', 'AutoMaster_TestSet_forTest.csv')
+
+DICT_INDEX = 500
